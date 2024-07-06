2137194c7071a223aa452cc5e0e9d6a88a86d8e1
Paula Cano <paulacanoroman@gmail.com>
2024-07-06 10:32:52 +0200

    feat: show table data and fix tbody as a child

--- prueba1/pages/components/TableDataPosts.jsx
+++ prueba1/pages/components/TableDataPosts.jsx
@@ -1,8 +1,48 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import ModalMaterial from './ModalMaterial'
 
 const TableDataPosts = () => {
+
+  const [dataTable, setDataTable] = useState([])
+
+  useEffect(() => {
+    dataAPI()
+  }, [])
+  
+
+  const dataAPI = async ()=>{
+    try{
+      const url = `https://jsonplaceholder.typicode.com/posts`
+      const call = await fetch(url)
+      const data = await call.json()
+      setDataTable(data)
+      console.log(data)
+    }catch(err){console.log(err, "error detected")}
+  }
+
+  const dataFormatted = dataTable.map((element)=>{
+    return(
+                  <tr>
+                    <td key={element.id}>{element.id}</td>
+                    <td>{element.title}</td>
+                    <td></td>
+                  </tr>
+          )
+    
+  })
+
+
   return (
-    <div>TableDataPosts</div>
+    <>
+      <div>TableDataPosts</div>
+      <table>
+        <tbody>
+             {dataFormatted}
+        </tbody>
+      </table>
+      <ModalMaterial></ModalMaterial>
+    </>
+
   )
 }
 
