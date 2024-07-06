4b3a43ba47667d01bf478253d6576d4f8233105f
Paula Cano <paulacanoroman@gmail.com>
2024-07-06 11:37:41 +0200

    feat: delete last word and new component hour

---
+++ prueba1/pages/components/ActualHourButton.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ActualHourButton = () => {
+  return (
+    <div>ActualHourButton</div>
+  )
+}
+
+export default ActualHourButton
--- prueba1/pages/components/DelLastWord.jsx
+++ prueba1/pages/components/DelLastWord.jsx
@@ -2,15 +2,30 @@ import React, { useState } from 'react'
 
 const DelLastWord = () => {
 
-    const [sentence, setDentence] = useState([]);
+    const [sentence, setSentence] = useState("");
+
+    const deleteLastWord = ()=>{
+        const words = structuredClone(sentence).split(' ')
+        words.pop()
+        const allSentence = setSentence(words.join(' '))
+        return(
+            <h3>{allSentence}</h3>
+        )
+    }
 
 
 
   return (
     <div>
-        <input placeholder='Write a sentence' onChange={(e)=>{setDentence(e.target.value)}}></input>
+        <input placeholder='Write a sentence' onChange={(e)=>{setSentence(e.target.value)}}></input>
+        <br></br>
+        <button onClick={()=>{deleteLastWord()}}>Delete last word</button>
+        <br></br>
+        {sentence}
     </div>
   )
+
+
 }
 
 export default DelLastWord
